35ca27af57059fe7f2a736e561e0223c86df89d1
Daniel Farina <daniel@heroku.com>
2011-03-15 22:46:33 -0700

    Add setup.py and tag v0.1
    Signed-off-by: Daniel Farina <daniel@heroku.com>

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os.path
+
+# Version file managment scheme and graceful degredation for
+# setuptools borrowed and adapted from GitPython.
+try:
+	from setuptools import setup, find_packages
+except ImportError:
+	from ez_setup import use_setuptools
+	use_setuptools()
+	from setuptools import setup, find_packages
+
+v = open(os.path.join(os.path.dirname(__file__), 'VERSION'))
+VERSION = v.readline().strip()
+v.close()
+
+setup(
+    name = "WAL-E",
+    version = VERSION,
+    packages = find_packages(),
+
+    install_requires = ['argparse>=0.8'],
+
+    # metadata for upload to PyPI
+    author = "Daniel Farina",
+    author_email = "daniel@heroku.com",
+    description = "PostgreSQL WAL-shipping for S3",
+    license = "BSD",
+    keywords = "postgresql database backup",
+    url = "https://github.com/heroku/wal-e",
+
+    # install 
+    entry_points = {'console_scripts': ['wal-e = wal_e.wal_e:main']}
+)
