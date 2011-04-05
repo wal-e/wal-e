69b6652e3e804ccf1cef43a5ec157629bb0bfa5f
Daniel Farina <daniel@heroku.com>
2011-04-05 13:30:20 -0700

    Make argparse dependency optional if running Python 2.7
    Signed-off-by: Daniel Farina <daniel@heroku.com>

--- setup.py
+++ setup.py
@@ -15,12 +15,19 @@ v = open(os.path.join(os.path.dirname(__file__), 'VERSION'))
 VERSION = v.readline().strip()
 v.close()
 
+import sys
+
+if sys.version_info < (2, 7):
+    install_requires = ['argparse>=0.8']
+else:
+    install_requires = []
+
 setup(
     name = "WAL-E",
     version = VERSION,
     packages = find_packages(),
 
-    install_requires = ['argparse>=0.8'],
+    install_requires = install_requires,
 
     # metadata for upload to PyPI
     author = "Daniel Farina",
