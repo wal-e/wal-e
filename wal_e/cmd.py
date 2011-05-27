6870b97abcd195a7e603b3ca5816ff8979515588
Daniel Farina <daniel@heroku.com>
2011-05-27 14:43:31 -0700

    Fix external program check detection
    Fixed, or made buggy in a different way...
    
    Signed-off-by: Daniel Farina <daniel@heroku.com>

--- wal_e/cmd.py
+++ wal_e/cmd.py
@@ -90,7 +90,7 @@ def external_program_check(
                     # of this kind will terminate in this case.
                     proc.stdin.close()
                     proc.wait()
-            except IOError:
+            except OSError, ose:
                 could_not_run.append(program)
 
     if could_not_run:
