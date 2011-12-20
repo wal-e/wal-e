f74c392687d068acdb4c92ec18990296da62d909
Daniel Farina <drfarina@acm.org>
2011-12-20 14:08:55 -0800

    Use pytest tmpdir support
    Signed-off-by: Daniel Farina <drfarina@acm.org>

--- tests/test_log_help.py
+++ tests/test_log_help.py
@@ -1,11 +1,6 @@
-import os
-import tempfile
-
 import wal_e.log_help
 
-def test_nonexisting_socket():
-    td = tempfile.mkdtemp(prefix='wal_e_test')
-
+def test_nonexisting_socket(tmpdir):
     # Must not raise an exception, silently failing is preferred for
     # now.
-    wal_e.log_help.configure(syslog_address=os.path.join(td, 'bogus'))
+    wal_e.log_help.configure(syslog_address=tmpdir.join('bogus'))
