57f40b6bc3a5739eb9bfe582e7fb460bfa5523be
Daniel Farina <drfarina@acm.org>
2011-12-19 17:29:10 -0800

    Use py.test for test discovery and running
    Signed-off-by: Daniel Farina <drfarina@acm.org>

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
+import errno
 import os.path
 import sys
+
+from distutils.cmd import Command
 
 # Version file managment scheme and graceful degredation for
 # setuptools borrowed and adapted from GitPython.
@@ -19,9 +22,34 @@ VERSION = v.readline().strip()
 v.close()
 
 install_requires = ['gevent>=0.13.0', 'boto>=2.0']
+extras_require = {
+    'test':  ["pytest>=2.2.1", 'pytest-xdist>=1.8']
+    }
 
 if sys.version_info < (2, 7):
     install_requires.append('argparse>=0.8')
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import subprocess
+
+        try:
+            retcode = subprocess.call('py.test')
+        except EnvironmentError, e:
+            if e.errno == errno.ENOENT:
+                print >>sys.stderr, ('Could not find test runner, consider '
+                                     '"pip install pytest pytest-xdist".')
+        else:
+            raise SystemExit(retcode)
 
 setup(
     name="WAL-E",
@@ -29,6 +57,7 @@ setup(
     packages=find_packages(),
 
     install_requires=install_requires,
+    extras_require=extras_require,
 
     # metadata for upload to PyPI
     author="Daniel Farina",
@@ -37,6 +66,9 @@ setup(
     license="BSD",
     keywords="postgresql database backup",
     url="https://github.com/heroku/wal-e",
+
+    # run tests
+    cmdclass={'test': PyTest},
 
     # install
     entry_points={'console_scripts': ['wal-e=wal_e.cmd:main']})
--- tests.py
+++
@@ -1,8 +0,0 @@
-import doctest
-
-import wal_e.exception
-import wal_e.storage.s3_storage
-
-doctest.testmod(wal_e.exception)
-doctest.testmod(wal_e.storage.s3_storage)
-
