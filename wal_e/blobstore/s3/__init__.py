20ec6e401803ec4343a945c49af4e67d5394e6bd
Daniel Farina <daniel@heroku.com>
2013-11-01 14:57:20 -0700

    Fix __all__ to use strings
    Also alphabetize all the import and __all__ list lines, now that some
    of the lists of exported functions are getting long enough to be
    harder to scan.
    
    Alex Gaynor <alex.gaynor@gmail.com> caught this systematic mistake of
    mine in code review.

--- wal_e/blobstore/s3/__init__.py
+++ wal_e/blobstore/s3/__init__.py
@@ -3,7 +3,7 @@ from wal_e.blobstore.s3.s3_util import do_lzop_get
 from wal_e.blobstore.s3.s3_util import write_and_return_error
 
 __all__ = [
-    uri_put_file,
-    do_lzop_get,
-    write_and_return_error,
+    'do_lzop_get',
+    'uri_put_file',
+    'write_and_return_error',
 ]
--- wal_e/worker/wabs/__init__.py
+++ wal_e/worker/wabs/__init__.py
@@ -1,13 +1,13 @@
-from wal_e.worker.wabs.wabs_worker import TarPartitionLister
+from wal_e.worker.wabs.wabs_deleter import Deleter
 from wal_e.worker.wabs.wabs_worker import BackupFetcher
 from wal_e.worker.wabs.wabs_worker import BackupList
 from wal_e.worker.wabs.wabs_worker import DeleteFromContext
-from wal_e.worker.wabs.wabs_deleter import Deleter
+from wal_e.worker.wabs.wabs_worker import TarPartitionLister
 
 __all__ = [
-    Deleter,
-    TarPartitionLister,
-    BackupFetcher,
-    BackupList,
-    DeleteFromContext,
+    'BackupFetcher',
+    'BackupList',
+    'DeleteFromContext',
+    'Deleter',
+    'TarPartitionLister',
 ]
