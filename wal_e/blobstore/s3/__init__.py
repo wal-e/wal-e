d9274dafbfbc241eada5c14127722138dafb72df
Daniel Farina <daniel@heroku.com>
2013-11-16 19:32:35 -0800

    Alphabetize imports

--- wal_e/blobstore/s3/__init__.py
+++ wal_e/blobstore/s3/__init__.py
@@ -1,6 +1,6 @@
-from wal_e.blobstore.s3.s3_util import uri_put_file
-from wal_e.blobstore.s3.s3_util import uri_get_file
 from wal_e.blobstore.s3.s3_util import do_lzop_get
+from wal_e.blobstore.s3.s3_util import uri_get_file
+from wal_e.blobstore.s3.s3_util import uri_put_file
 from wal_e.blobstore.s3.s3_util import write_and_return_error
 
 __all__ = [
