ea0e55dcbd488ff1c5f32a4170021393d0249781
Brian Oldfield <brian@oldfield.io>
2013-11-20 07:53:55 -0800

    Adding retry logic around WABS chunk upload and download.

--- wal_e/blobstore/wabs/wabs_util.py
+++ wal_e/blobstore/wabs/wabs_util.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import gevent
+import errno
 import socket
 from urlparse import urlparse
 import traceback
@@ -28,6 +30,47 @@ def uri_put_file(creds, uri, fp, content_encoding=None):
     assert fp.tell() == 0
     assert uri.startswith('wabs://')
 
+    def log_upload_failures_on_error(exc_tup, exc_processor_cxt):
+        def standard_detail_message(prefix=''):
+            return (prefix + '  There have been {n} attempts to upload  '
+                    'file {url} so far.'.format(n=exc_processor_cxt, url=uri))
+        typ, value, tb = exc_tup
+        del exc_tup
+
+        # Screen for certain kinds of known-errors to retry from
+        if issubclass(typ, socket.error):
+            socketmsg = value[1] if isinstance(value, tuple) else value
+
+            logger.info(
+                msg='Retrying upload because of a socket error',
+                detail=standard_detail_message(
+                    "The socket error's message is '{0}'."
+                    .format(socketmsg)))
+        else:
+            # For all otherwise untreated exceptions, report them as a
+            # warning and retry anyway -- all exceptions that can be
+            # justified should be treated and have error messages
+            # listed.
+            logger.warning(
+                msg='retrying file upload from unexpected exception',
+                detail=standard_detail_message(
+                    'The exception type is {etype} and its value is '
+                    '{evalue} and its traceback is {etraceback}'
+                    .format(etype=typ, evalue=value,
+                            etraceback=''.join(traceback.format_tb(tb)))))
+
+        # Help Python GC by resolving possible cycles
+        del tb
+
+    # Because we're uploading in chunks, catch rate limiting and
+    # connection errors which occur for each individual chunk instead of
+    # failing the whole file and restarting.
+    @retry(retry_with_count(log_upload_failures_on_error))
+    def upload_chunk(chunk, block_id):
+        check_sum = base64.encodestring(md5(chunk).digest()).strip('\n')
+        conn.put_block(url_tup.netloc, url_tup.path, chunk,
+                       block_id, content_md5=check_sum)
+
     url_tup = urlparse(uri)
     kwargs = dict(x_ms_blob_type='BlockBlob')
     if content_encoding is not None:
@@ -36,11 +79,7 @@ def uri_put_file(creds, uri, fp, content_encoding=None):
     conn = BlobService(creds.account_name, creds.account_key, protocol='https')
     conn.put_blob(url_tup.netloc, url_tup.path, '', **kwargs)
 
-    def _upload_chunk(chunk, block_id):
-        check_sum = base64.encodestring(md5(chunk).digest()).strip('\n')
-        conn.put_block(url_tup.netloc, url_tup.path, chunk,
-                       block_id, content_md5=check_sum)
-
+    # WABS requires large files to be uploaded in 4MB chunks
     block_ids = []
     length, index = 0, 0
     pool_size = os.getenv('WABS_UPLOAD_POOL_SIZE', 5)
@@ -51,7 +90,7 @@ def uri_put_file(creds, uri, fp, content_encoding=None):
             length += len(data)
             block_id = base64.b64encode(str(index))
             p.wait_available()
-            p.spawn(_upload_chunk, data, block_id)
+            p.spawn(upload_chunk, data, block_id)
             block_ids.append(block_id)
             index += 1
         else:
@@ -80,10 +119,31 @@ def uri_get_file(creds, uri, conn=None):
 
     ret_size = 0
     data = ''
+    # WABS requires large files to be downloaded in 4MB chunks
     while ret_size < blob_size:
         ms_range = 'bytes={}-{}'.format(ret_size,
                                         ret_size + WABS_CHUNK_SIZE - 1)
-        part = conn.get_blob(url_tup.netloc, url_tup.path, x_ms_range=ms_range)
+        while True:
+            # Because we're downloading in chunks, catch rate limiting and
+            # connection errors here instead of letting them bubble up to the
+            # @retry decorator so that we don't have to start downloading the
+            # whole file over again.
+            try:
+                part = conn.get_blob(url_tup.netloc,
+                                     url_tup.path,
+                                     x_ms_range=ms_range)
+            except Exception, e:
+                if e.errno in (errno.EBUSY, errno.ECONNRESET):
+                    logger.warning(
+                        msg="retrying after encountering exception",
+                        detail=("Exception traceback:\n{0}".format(
+                            traceback.format_exception(*sys.exc_info()))),
+                        hint="")
+                    gevent.sleep(30)
+                else:
+                    raise e
+            else:
+                break
         length = len(part)
         ret_size += length
         data += part
