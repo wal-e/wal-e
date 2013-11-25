4d56d3e796c993fc59043cd8643c6582970c0c59
Dan Robinson <dan@heapanalytics.com>
2013-11-25 00:32:37 -0800

    Adds a test that creating a TarMemberTooBigError works.

---
+++ tests/test_exceptions.py
@@ -0,0 +1,11 @@
+from wal_e.tar_partition import TarMemberTooBigError
+
+
+def test_tar_member_too_big_error():
+    # Must not raise an exception.
+    a = TarMemberTooBigError("file.dat", 100, 150)
+    assert a.msg == 'Attempted to archive a file that is too large.'
+    hint = ('There is a file in the postgres database directory that '
+            'is larger than 100 bytes. If no such file exists, please '
+            'report this as a bug. In particular, check file.dat.')
+    assert a.hint == hint
