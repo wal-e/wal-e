517af987ba14571d165c8fe93e8459f9bc840f0a
Dan Robinson <dan@heapanalytics.com>
2013-11-25 00:39:13 -0800

    Adds apparent file size to TarMemberTooBigError hint.
    Updates tests correspondingly.

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,3 +1,7 @@
+# Test exception construction
+#
+# Flush out simple typo crashes and problems with occasional changes
+# in the exception base classes.
 from wal_e.tar_partition import TarMemberTooBigError
 
 
@@ -7,5 +11,6 @@ def test_tar_member_too_big_error():
     assert a.msg == 'Attempted to archive a file that is too large.'
     hint = ('There is a file in the postgres database directory that '
             'is larger than 100 bytes. If no such file exists, please '
-            'report this as a bug. In particular, check file.dat.')
+            'report this as a bug. In particular, check file.dat, '
+            'which appears to be 150 bytes.')
     assert a.hint == hint
