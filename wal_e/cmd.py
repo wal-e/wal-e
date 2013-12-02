wal-e/wal-e

commit e90973ae83b56197851caa1c5247fab255b25f40

All tablespaces are now backedup alongside base postgres data.

wal_e/cmd.py
@@ -266,6 +266,19 @@ def build_parser():
     # backup-fetch operator section
     backup_fetch_parser.add_argument('BACKUP_NAME',
                                      help='the name of the backup to fetch')
+    backup_fetch_parser.add_argument(
+        '--blind-restore',
+        help='Restore from backup without verification of tablespace symlinks',
+        dest='blind_restore',
+        action='store_true',
+        default=False)
+
+    backup_fetch_parser.add_argument(
+        '--restore-spec',
+        help=('Specification for the directory structure of the database '
+              'restoration (optional, see README for more information).'),
+        type=str,
+        default=None)
 
     # backup-list operator section
     backup_list_parser.add_argument(
@@ -408,6 +421,8 @@ def main(argv=None):
             backup_cxt.database_fetch(
                 args.PG_CLUSTER_DIRECTORY,
                 args.BACKUP_NAME,
+                blind_restore=args.blind_restore,
+                restore_spec=args.restore_spec,
                 pool_size=args.pool_size)
         elif subcommand == 'backup-list':
             backup_cxt.backup_list(query=args.QUERY, detail=args.detail)

wal_e/worker/base.py
@@ -1,7 +1,6 @@
 import gevent
 from gevent import queue
 import re
-import json
 
 from wal_e import exception
 from wal_e import storage
@@ -172,28 +171,19 @@ class _BackupList(object):
                     # request-per.
                     groups = match.groupdict()
 
-                    detail_dict = {'wal_segment_backup_stop': None,
-                                   'wal_segment_offset_backup_stop': None,
-                                   'expanded_size_bytes': None}
-                    if self.detail:
-                        try:
-                            # This costs one web request
-                            detail_dict.update(
-                                json.loads(self._backup_detail(key)))
-                        except gevent.Timeout:
-                            # NB: do *not* overwite "key" in this
-                            # scope, which is being used to mean a "s3
-                            # key", as this will cause later code to
-                            # blow up.
-                            for k in detail_dict:
-                                detail_dict[k] = 'timeout'
-
-                    info = storage.BackupInfo(
+                    info = storage.get_backup_info(
+                        self.layout,
                         name='base_{filename}_{offset}'.format(**groups),
                         last_modified=self.layout.key_last_modified(key),
                         wal_segment_backup_start=groups['filename'],
-                        wal_segment_offset_backup_start=groups['offset'],
-                        **detail_dict)
+                        wal_segment_offset_backup_start=groups['offset'])
+
+                    if self.detail:
+                        try:
+                            # This costs one web request
+                            info.load_detail(self.conn)
+                        except gevent.Timeout:
+                            pass
 
                     yield info
 

wal_e/storage/base.py
@@ -42,24 +42,36 @@ class SegmentNumber(collections.namedtuple('SegmentNumber',
         assert len(self.seg) == 8
         return int(self.log + self.seg, 16)
 
+OBSOLETE_VERSIONS = frozenset(('004', '003', '002', '001', '000'))
+
+SUPPORTED_STORE_SCHEMES = ('s3', 'wabs')
+
+
 # Exhaustively enumerates all possible metadata about a backup.  These
 # may not always all be filled depending what access method is used to
 # get information, in which case the unfilled items should be given a
 # None value.  If an item was intended to be fetch, but could not be
 # after some number of retries and timeouts, the field should be
 # filled with the string 'timeout'.
-BackupInfo = collections.namedtuple('BackupInfo',
-                                    ['name',
-                                     'last_modified',
-                                     'expanded_size_bytes',
-                                     'wal_segment_backup_start',
-                                     'wal_segment_offset_backup_start',
-                                     'wal_segment_backup_stop',
-                                     'wal_segment_offset_backup_stop'])
+class BackupInfo(object):
+    _fields = ['name',
+               'last_modified',
+               'expanded_size_bytes',
+               'wal_segment_backup_start',
+               'wal_segment_offset_backup_start',
+               'wal_segment_backup_stop',
+               'wal_segment_offset_backup_stop']
 
-OBSOLETE_VERSIONS = frozenset(('004', '003', '002', '001', '000'))
+    def __init__(self, **kwargs):
+        for field in self._fields:
+            setattr(self, field, kwargs.get(field, None))
 
-SUPPORTED_STORE_SCHEMES = ('s3', 'wabs')
+        self.layout = kwargs['layout']
+        self.spec = kwargs.get('spec', None)
+        self._details_loaded = False
+
+    def load_detail(self, conn):
+        raise NotImplementedError()
 
 
 class StorageLayout(object):
@@ -166,8 +178,10 @@ class StorageLayout(object):
 
     def basebackup_sentinel(self, backup_info):
         self._error_on_unexpected_version()
-        return (self.basebackup_directory(backup_info) +
-                '_backup_stop_sentinel.json')
+        basebackup = self.basebackup_directory(backup_info)
+        # need to strip the trailing slash on the base backup dir
+        # to correctly point to the sentinel
+        return (basebackup.rstrip('/') + '_backup_stop_sentinel.json')
 
     def basebackup_tar_partition_directory(self, backup_info):
         self._error_on_unexpected_version()
@@ -198,3 +212,14 @@ class StorageLayout(object):
         if hasattr(key, 'last_modified'):
             return key.last_modified
         return key.properties.last_modified
+
+
+def get_backup_info(layout, **kwargs):
+    kwargs['layout'] = layout
+    if layout.is_s3:
+        from wal_e.storage.s3_storage import S3BackupInfo
+        bi = S3BackupInfo(**kwargs)
+    elif layout.is_wabs:
+        from wal_e.storage.wabs_storage import WABSBackupInfo
+        bi = WABSBackupInfo(**kwargs)
+    return bi

wal_e/blobstore/s3/__init__.py
@@ -1,9 +1,11 @@
 from wal_e.blobstore.s3.s3_util import uri_put_file
+from wal_e.blobstore.s3.s3_util import uri_get_file
 from wal_e.blobstore.s3.s3_util import do_lzop_get
 from wal_e.blobstore.s3.s3_util import write_and_return_error
 
 __all__ = [
     'do_lzop_get',
     'uri_put_file',
+    'uri_get_file',
     'write_and_return_error',
 ]

wal_e/blobstore/s3/s3_util.py
@@ -24,17 +24,19 @@ if not boto.config.has_option('Boto', 'http_socket_timeout'):
     boto.config.set('Boto', 'http_socket_timeout', '5')
 
 
-def _uri_to_key(access_key, secret_key, uri):
+def _uri_to_key(access_key, secret_key, uri, conn=None):
     assert uri.startswith('s3://')
     url_tup = urlparse(uri)
     bucket_name = url_tup.netloc
     cinfo = calling_format.from_store_name(bucket_name)
-    conn = cinfo.connect(access_key, secret_key)
+    if conn is None:
+        conn = cinfo.connect(access_key, secret_key)
     bucket = boto.s3.bucket.Bucket(connection=conn, name=bucket_name)
     return boto.s3.key.Key(bucket=bucket, name=url_tup.path)
 
 
-def uri_put_file(access_key, secret_access, uri, fp, content_encoding=None):
+def uri_put_file(access_key, secret_access, uri,
+                 fp, content_encoding=None, conn=None):
     # Per Boto 2.2.2, which will only read from the current file
     # position to the end.  This manifests as successfully uploaded
     # *empty* keys in S3 instead of the intended data because of how
@@ -45,13 +47,18 @@ def uri_put_file(access_key, secret_access, uri, fp, content_encoding=None):
     # in mind, assert it as a precondition for using this procedure.
     assert fp.tell() == 0
 
-    k = _uri_to_key(access_key, secret_access, uri)
+    k = _uri_to_key(access_key, secret_access, uri, conn=conn)
 
     if content_encoding is not None:
         k.content_type = content_encoding
 
     k.set_contents_from_file(fp)
     return k
+
+
+def uri_get_file(access_key, secret_access, uri, conn=None):
+    k = _uri_to_key(access_key, secret_access, uri, conn=conn)
+    return k.get_contents_as_string()
 
 
 def do_lzop_get(access_key, secret_key, url, path, decrypt):

wal_e/blobstore/wabs/__init__.py
@@ -1,9 +1,11 @@
 from wal_e.blobstore.wabs.wabs_util import do_lzop_get
 from wal_e.blobstore.wabs.wabs_util import uri_put_file
+from wal_e.blobstore.wabs.wabs_util import uri_get_file
 from wal_e.blobstore.wabs.wabs_util import write_and_return_error
 
 __all__ = [
     'do_lzop_get',
     'uri_put_file',
+    'uri_get_file',
     'write_and_return_error',
 ]

wal_e/blobstore/wabs/wabs_util.py
@@ -42,6 +42,15 @@ def uri_put_file(access_key, secret_key, uri, fp, content_encoding=None):
     return _Key(size=len(data))
 
 
+def uri_get_file(access_key, secret_key, uri, conn=None):
+    assert uri.startswith('wabs://')
+    url_tup = urlparse(uri)
+
+    if conn is None:
+        conn = BlobService(access_key, secret_key, protocol='https')
+    return conn.get_blob(url_tup.netloc, url_tup.path)
+
+
 def do_lzop_get(access_key, secret_key, url, path, decrypt):
     """
     Get and decompress a S3 URL

wal_e/operator/backup.py
@@ -21,6 +21,8 @@ from wal_e.worker import (WalSegment,
                           do_lzop_get)
 import wal_e.log_help as log_help
 
+# File mode on directories created during restore process
+DEFAULT_DIR_MODE = 0700
 # Provides guidence in object names as to the version of the file
 # structure.
 FILE_STRUCTURE_VERSION = storage.CURRENT_VERSION
@@ -47,7 +49,7 @@ class Backup(object):
 
         """
         import csv
-        from wal_e.storage import BackupInfo
+        from wal_e.storage.base import BackupInfo
         bl = self._backup_list(detail)
 
         # If there is no query, return an exhaustive list, otherwise
@@ -61,12 +63,13 @@ class Backup(object):
         w_csv = csv.writer(sys.stdout, dialect='excel-tab')
         w_csv.writerow(BackupInfo._fields)
 
-        for backup_info in bl_iter:
-            w_csv.writerow(backup_info)
+        for bi in bl_iter:
+            w_csv.writerow([getattr(bi, k) for k in BackupInfo._fields])
 
         sys.stdout.flush()
 
-    def database_fetch(self, pg_cluster_dir, backup_name, pool_size):
+    def database_fetch(self, pg_cluster_dir, backup_name,
+                       blind_restore, restore_spec, pool_size):
         if os.path.exists(os.path.join(pg_cluster_dir, 'postmaster.pid')):
             hint = ('Shut down postgres. If there is a stale lockfile, '
                     'then remove it after being very sure postgres is not '
@@ -99,7 +102,32 @@ class Backup(object):
         assert backups[0] is not None
 
         backup_info = backups[0]
+        backup_info.load_detail(self.new_connection())
         layout.basebackup_tar_partition_directory(backup_info)
+
+        if restore_spec is not None:
+            if restore_spec != 'SOURCE':
+                if not os.path.isfile(restore_spec):
+                    raise UserException(
+                        msg='Restore specification does not exist',
+                        detail='File not found: %s'.format(restore_spec),
+                        hint=('Provide valid json-formatted restoration '
+                              'specification, or pseudo-name "SOURCE" to '
+                              'restore using the specification from the '
+                              'backup progenitor.'))
+                with open(restore_spec, 'r') as fs:
+                    spec = json.load(fs)
+                backup_info.spec.update(spec)
+            if 'base_prefix' not in spec or not spec['base_prefix']:
+                backup_info.spec['base_prefix'] = pg_cluster_dir
+            self._build_restore_paths(backup_info.spec)
+        else:
+            # If the user hasn't passed in a restoration specification
+            # use pg_cluster_dir as the resore prefix
+            backup_info.spec['base_prefix'] = pg_cluster_dir
+
+        if not blind_restore:
+            self._verify_restore_paths(backup_info.spec)
 
         connections = []
         for i in xrange(pool_size):
@@ -112,7 +140,8 @@ class Backup(object):
         fetchers = []
         for i in xrange(pool_size):
             fetchers.append(self.worker.BackupFetcher(
-                connections[i], layout, backup_info, pg_cluster_dir,
+                connections[i], layout, backup_info,
+                backup_info.spec['base_prefix'],
                 (self.gpg_key_id is not None)))
         assert len(fetchers) == pool_size
 
@@ -163,9 +192,10 @@ class Backup(object):
                 start_backup_info = ctrl_data.last_xlog_file_name_and_offset()
                 version = ctrl_data.pg_version()
 
-            uploaded_to, expanded_size_bytes = self._upload_pg_cluster_dir(
+            ret_tuple = self._upload_pg_cluster_dir(
                 start_backup_info, data_directory, version=version, *args,
                 **kwargs)
+            spec, uploaded_to, expanded_size_bytes = ret_tuple
             upload_good = True
         finally:
             if not upload_good:
@@ -197,7 +227,8 @@ class Backup(object):
                      stop_backup_info['file_name'],
                  'wal_segment_offset_backup_stop':
                      stop_backup_info['file_offset'],
-                 'expanded_size_bytes': expanded_size_bytes},
+                 'expanded_size_bytes': expanded_size_bytes,
+                 'spec': spec},
                 sentinel_content)
 
             # XXX: should use the storage operators.
@@ -349,7 +380,7 @@ class Backup(object):
         (which affect upload throughput) would help.
 
         """
-        parts = tar_partition.partition(pg_cluster_dir)
+        spec, parts = tar_partition.partition(pg_cluster_dir)
 
         backup_prefix = ('{0}/basebackups_{1}/'
                          'base_{file_name}_{file_offset}'
@@ -377,6 +408,7 @@ class Backup(object):
                      self.secret_key,
                      extended_version_url, StringIO(version),
                      content_encoding='text/plain')
+
         logger.info(msg='postgres version metadata upload complete')
 
         uploader = PartitionUploader(
@@ -397,7 +429,7 @@ class Backup(object):
         # raised to signal failure of the upload.
         pool.join()
 
-        return backup_prefix, total_size
+        return spec, backup_prefix, total_size
 
     def _exception_gather_guard(self, fn):
         """
@@ -416,6 +448,41 @@ class Backup(object):
                 self.exceptions.append(e)
 
         return wrapper
+
+    def _build_restore_paths(self, restor_spec):
+        path_prefix = restor_spec['base_prefix']
+        tblspc_prefix = os.path.join(path_prefix, 'pg_tblspc')
+
+        if not os.path.isdir(path_prefix):
+            os.mkdir(path_prefix, DEFAULT_DIR_MODE)
+            os.mkdir(tblspc_prefix, DEFAULT_DIR_MODE)
+
+        for tblspc in restor_spec['tablespaces']:
+            dest = os.path.join(path_prefix,
+                                restor_spec[tblspc]['link'])
+            source = restor_spec[tblspc]['loc']
+            if not os.path.isdir(source):
+                os.mkdir(source, DEFAULT_DIR_MODE)
+            os.symlink(source, dest)
+
+    def _verify_restore_paths(self, restor_spec):
+        path_prefix = restor_spec['base_prefix']
+        bad_links = []
+        for tblspc in restor_spec['tablespaces']:
+            tblspc_link = os.path.join(path_prefix, 'pg_tblspc', tblspc)
+            valid = os.path.islink(tblspc_link) and os.path.isdir(tblspc_link)
+            if not valid:
+                bad_links.append(tblspc)
+
+        if bad_links:
+            raise UserException(
+                msg='Symlinks for some tablespaces not found or created.',
+                detail=('Symlinks for the following tablespaces were not '
+                        'found: {spaces}'.format(spaces=', '.join(bad_links))),
+                hint=('Ensure all required symlinks are created prior to '
+                      'running backup-fetch, or use --blind-restore to '
+                      'ignore symlinking. Alternatively supply a restore '
+                      'spec to have WAL-E create tablespace symlinks for you'))
 
 
 def get_backup_context(layout, *args):

commit b9c0ebc1d9232a8007493b3bd88b3ba934b15d5d

Fixes references to storage.base.SegmentNumber.
These seem to have been broken by e90973ae83b56197851caa1c5247fab255b25f40.

wal_e/cmd.py
@@ -155,7 +155,7 @@ def external_program_check(
 
 def extract_segment(text_with_extractable_segment):
     from wal_e.storage import BASE_BACKUP_REGEXP
-    from wal_e.storage import SegmentNumber
+    from wal_e.storage.base import SegmentNumber
 
     match = re.match(BASE_BACKUP_REGEXP, text_with_extractable_segment)
     if match is None:

wal_e/worker/base.py
@@ -221,7 +221,7 @@ class _DeleteFromContext(object):
             assert False
 
     def _groupdict_to_segment_number(self, d):
-        return storage.SegmentNumber(log=d['log'], seg=d['seg'])
+        return storage.base.SegmentNumber(log=d['log'], seg=d['seg'])
 
     def _delete_if_before(self, delete_horizon_segment_number,
                             scanned_segment_number, key, type_of_thing):