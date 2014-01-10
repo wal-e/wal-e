107a673261a12cd4653828b2844ceccb0ca8f3d6
Daniel Farina <daniel@heroku.com>
2014-01-10 14:45:36 -0800

    Move pg_xlog test fixture to another file
    Done in preparation to have it be shared with other integration
    testing, and to communicate that it is a mechanism used by multiple
    consumers (soon).

--- tests/test_wal_segment.py
+++ tests/test_wal_segment.py
@@ -1,40 +1,11 @@
 import pytest
 
+from stage_pgxlog import pg_xlog
 from wal_e import worker
 from wal_e import exception
 
-
-class PgXlog(object):
-    """Test utility for staging a pg_xlog directory."""
-
-    def __init__(self, cluster):
-        self.cluster = cluster
-
-        self.pg_xlog = cluster.join('pg_xlog')
-        self.pg_xlog.ensure(dir=True)
-
-        self.status = self.pg_xlog.join('archive_status')
-        self.status.ensure(dir=True)
-
-    def touch(self, name, status):
-        assert status in ('.ready', '.done')
-
-        self.pg_xlog.join(name).ensure(file=True)
-        self.status.join(name + status).ensure(file=True)
-
-    def assert_exists(self, name, status):
-        assert status in ('.ready', '.done')
-
-        assert self.pg_xlog.join(name).check(exists=1)
-        assert self.status.join(name + status).check(exists=1)
-
-
-@pytest.fixture()
-def pg_xlog(tmpdir, monkeypatch):
-    """Set up xlog utility functions and change directories."""
-    monkeypatch.chdir(tmpdir)
-
-    return PgXlog(tmpdir)
+# Quiet pyflakes about pytest fixtures.
+assert pg_xlog
 
 
 def make_segment(num, **kwargs):
