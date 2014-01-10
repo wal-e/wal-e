35f78a55bf76914d2e6f5f4971eebe5ddf4218ee
Matthew Hooker <mwhooker@gmail.com>
2014-01-10 13:58:35 -0800

    add --aws-instance-profile agument.
    If this argument is provided, we'll create a special credential
    that knows how to build itself from the instance profile.

--- wal_e/cmd.py
+++ wal_e/cmd.py
@@ -183,6 +183,12 @@ def build_parser():
                         'variable. If both are defined, the one defined'
                         'in the programs arguments takes precedence.')
 
+    parser.add_argument('--aws-instance-profile',
+                        help='Use the IAM Instance Profile associated '
+                        'with this instance to authenticate with the s3 API. '
+                        'This argument takes precedence over '
+                        '--aws-access-key-id')
+
     parser.add_argument('--s3-prefix',
                         help='S3 prefix to run all commands against.  '
                         'Can also be defined via environment variable '
@@ -387,25 +393,33 @@ def configure_backup_cxt(args):
     # backend data stores, yielding value adhering to the
     # 'operator.Backup' protocol.
     if store.is_s3:
-        access_key = args.aws_access_key_id or os.getenv('AWS_ACCESS_KEY_ID')
-        if access_key is None:
-            raise UserException(
-                msg='AWS Access Key credential is required but not provided',
-                hint=(_opt_env_hint('aws-access-key-id')))
-
-        secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-        if secret_key is None:
-            raise UserException(
-                msg='AWS Secret Key credential is required but not provided',
-                hint=_env_hint('aws-secret-access-key'))
-
-        security_token = os.getenv('AWS_SECURITY_TOKEN')
-
         from wal_e.blobstore import s3
+
+        if args.aws_instance_profile:
+            creds = s3.InstanceProfileCredentials()
+            # TODO: if we can't find an instance profile, hint to the
+            # user that they may need to configure that correctly.
+        else:
+            access_key = (args.aws_access_key_id or
+                          os.getenv('AWS_ACCESS_KEY_ID'))
+            if access_key is None:
+                raise UserException(
+                    msg='AWS Access Key credential is required '
+                    'but not provided',
+                    hint=(_opt_env_hint('aws-access-key-id')))
+
+            secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+            if secret_key is None:
+                raise UserException(
+                    msg='AWS Secret Key credential is required '
+                    'but not provided',
+                    hint=_env_hint('aws-secret-access-key'))
+
+            security_token = os.getenv('AWS_SECURITY_TOKEN')
+
+            creds = s3.Credentials(access_key, secret_key, security_token)
+
         from wal_e.operator.s3_operator import S3Backup
-
-        creds = s3.Credentials(access_key, secret_key, security_token)
-
         return S3Backup(store, creds, gpg_key_id)
     elif store.is_wabs:
         account_name = args.wabs_account_name or os.getenv('WABS_ACCOUNT_NAME')
