06b1d406fc7f85dda4be3860b94c6978cb3a2137
Matthew Hooker <mwhooker@gmail.com>
2014-01-10 16:25:04 -0800

    flat is better than nested.
    Fixes syntax error

--- wal_e/blobstore/s3/s3_credentials.py
+++ wal_e/blobstore/s3/s3_credentials.py
@@ -1,5 +1,24 @@
-from wal_e.blobstore.s3.instance_profile_provider import InstanceProfileProvider
 from boto.provider import Provider as DefaultS3Provider
+
+from wal_e.exception import UserException
+
+
+class InstanceProfileProvider(DefaultS3Provider):
+    """Provides the S3Connection class with credentials discovered
+    through the aws metadata store.
+
+    """
+
+    def get_credentials(self, access_key=None, secret_key=None,
+                        security_token=None):
+        if self.MetadataServiceSupport[self.name]:
+            self._populate_keys_from_metadata_server()
+
+        if not self._secret_key:
+            raise UserException('Could not retrieve secret key from instance '
+                                'profile.',
+                                hint='Check that your instance has an IAM '
+                                'profile or set --aws-acces-key-id')
 
 
 class Credentials(object):
