870af1e04d49342291ee0c98dbff353d3e3996db
Daniel Farina <daniel@heroku.com>
2014-02-18 13:50:49 -0800

    Use larger buffers for temporary file reads for performance
    As witnessed this would otherwise perform small 8K reads by default.
    Reduce the amount of syscall thrashing and memory shuffling by Python
    by asking for larger amounts from the kernel at a time.
    
    It might be better if one could use the "async I/O via cat" trick.
    That would allow the program to schedule concurrent operations more
    aggressively.  However, that is hard to do because of boto's
    key.set_contents_from_file assumes a non-stream file object with .tell
    and .seek operators.

--- wal_e/worker/worker_util.py
+++ wal_e/worker/worker_util.py
@@ -1,6 +1,7 @@
 import tempfile
 import time
 
+from wal_e import pipebuf
 from wal_e import storage
 from wal_e.blobstore import get_blobstore
 from wal_e import pipeline
@@ -26,7 +27,8 @@ def do_lzop_put(creds, url, local_path, gpg_key):
     assert url.endswith('.lzo')
     blobstore = get_blobstore(storage.StorageLayout(url))
 
-    with tempfile.NamedTemporaryFile(mode='r+b') as tf:
+    with tempfile.NamedTemporaryFile(
+            mode='r+b', bufsize=pipebuf.PIPE_BUF_BYTES) as tf:
         pl = pipeline.get_upload_pipeline(
             open(local_path, 'r'), tf, gpg_key=gpg_key)
         pl.finish()
