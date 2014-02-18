43e56a5d845813dadf9c942c065e429187c25358
Daniel Farina <daniel@heroku.com>
2014-02-18 11:48:56 -0800

    Force use of response buffering via monkeypatch
    Since Python 2.7, getresponse supports an undocumented
    'buffering' keyword argument that can avoid many small system
    calls.

--- wal_e/cmd.py
+++ wal_e/cmd.py
@@ -22,6 +22,18 @@ gevent_monkey()
 #
 # Only Python 2.7+ possesses the 'ciphers' keyword to wrap_socket.
 if sys.version_info >= (2, 7):
+    def getresponse_monkey():
+        import httplib
+        original = httplib.HTTPConnection.getresponse
+
+        def monkey(*args, **kwargs):
+            kwargs['buffering'] = True
+            return original(*args, **kwargs)
+
+        httplib.HTTPConnection.getresponse = monkey
+
+    getresponse_monkey()
+
     def ssl_monkey():
         import ssl
 
