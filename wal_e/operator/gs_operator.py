5a743e48a78328179939687687d5707a6a9917ee
Matt Wright <matt@nobien.net>
2015-08-19 15:35:31 -0400

    Fix gs operator

--- wal_e/operator/gs_operator.py
+++ wal_e/operator/gs_operator.py
@@ -1,6 +1,6 @@
-from wal_e.blobstore.swift import calling_format
+from wal_e.blobstore.gs import calling_format
 from wal_e.operator.backup import Backup
-from wal_e.worker.swift import swift_worker
+from wal_e.worker.gs import gs_worker
 
 
 class GSBackup(Backup):
@@ -11,4 +11,4 @@ class GSBackup(Backup):
     def __init__(self, layout, creds, gpg_key_id):
         super(GSBackup, self).__init__(layout, creds, gpg_key_id)
         self.cinfo = calling_format
-        self.worker = swift_worker
+        self.worker = gs_worker
