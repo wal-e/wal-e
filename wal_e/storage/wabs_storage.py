2ca7b78188328a9ffcfe4d655e3e171f6d11314d
Monami-ya Support <support@monami-ya.com>
2016-08-11 03:37:05 +0000

    Convert bytes to str.
    On Python3, it does not convert bytes to str implicitly.

--- wal_e/storage/wabs_storage.py
+++ wal_e/storage/wabs_storage.py
@@ -13,7 +13,7 @@ class WABSBackupInfo(BackupInfo):
             bucket=self.layout.store_name(),
             path=self.layout.basebackup_sentinel(self))
         from wal_e.blobstore import wabs
-        data = wabs.uri_get_file(None, uri, conn=conn)
+        data = wabs.uri_get_file(None, uri, conn=conn).decode('utf-8')
         data = json.loads(data)
         for (k, v) in list(data.items()):
             setattr(self, k, v)
