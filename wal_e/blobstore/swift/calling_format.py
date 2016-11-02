be922a50f4f8f93d5a9e0ab1c61521414700f4d8
Joel E. Svensson <joeles_93@hotmail.com>
2016-11-02 09:57:45 +0100

    Add environment variables for Swift Keystone v3

--- wal_e/blobstore/swift/calling_format.py
+++ wal_e/blobstore/swift/calling_format.py
@@ -13,6 +13,16 @@ def connect(creds):
         tenant_name=creds.tenant_name,
         os_options={
             "region_name": creds.region,
-            "endpoint_type": creds.endpoint_type
+            "endpoint_type": creds.endpoint_type,
+            "domain_id": creds.domain_id,
+            "domain_name": creds.domain_name,
+            "tenant_id": creds.tenant_id,
+            "user_id": creds.user_id,
+            "user_domain_id": creds.user_domain_id,
+            "user_domain_name": creds.user_domain_name,
+            "project_id": creds.project_id,
+            "project_name": creds.project_name,
+            "project_domain_id": creds.project_domain_id,
+            "project_domain_name": creds.project_domain_name,
         }
     )
--- wal_e/blobstore/swift/credentials.py
+++ wal_e/blobstore/swift/credentials.py
@@ -1,6 +1,8 @@
 class Credentials(object):
     def __init__(self, authurl, user, password, tenant_name, region,
-            endpoint_type, auth_version):
+            endpoint_type, auth_version, domain_id, domain_name, tenant_id,
+            user_id, user_domain_id, user_domain_name, project_id,
+            project_name, project_domain_id, project_domain_name):
         self.authurl = authurl
         self.user = user
         self.password = password
@@ -8,3 +10,13 @@ class Credentials(object):
         self.region = region
         self.endpoint_type = endpoint_type
         self.auth_version = auth_version
+        self.domain_id = domain_id
+        self.domain_name = domain_name
+        self.tenant_id = tenant_id
+        self.user_id = user_id
+        self.user_domain_id = user_domain_id
+        self.user_domain_name = user_domain_name
+        self.project_id = project_id
+        self.project_name = project_name
+        self.project_domain_id = project_domain_id
+        self.project_domain_name = project_domain_name
