bf264d5683c7fcab69e117f235fbe16298ac90b8
Ayush Goyal <ayush@helpshift.com>
2016-11-04 00:34:11 +0530

    Fix infinite retry while deleting missing resource in WABS

--- wal_e/worker/wabs/wabs_deleter.py
+++ wal_e/worker/wabs/wabs_deleter.py
@@ -1,5 +1,20 @@
 from wal_e import retries
+from wal_e import log_help
 from wal_e.worker.base import _Deleter
+
+try:
+    # New class name in the Azure SDK sometime after v1.0.
+    #
+    # See
+    # https://github.com/Azure/azure-sdk-for-python/blob/master/ChangeLog.txt
+    from azure.common import AzureMissingResourceHttpError
+except ImportError:
+    # Backwards compatbility for older Azure drivers.
+    from azure import WindowsAzureMissingResourceError \
+        as AzureMissingResourceHttpError
+
+
+logger = log_help.WalELogger(__name__)
 
 
 class Deleter(_Deleter):
@@ -14,4 +29,10 @@ class Deleter(_Deleter):
         # Azure Blob Service has no concept of mass-delete, so we must nuke
         # each blob one-by-one...
         for blob in page:
-            self.wabs_conn.delete_blob(self.container, blob.name)
+            try:
+                self.wabs_conn.delete_blob(self.container, blob.name)
+            except AzureMissingResourceHttpError:
+                logger.warning(
+                    msg='failed while deleting resource',
+                    detail='Blob {0} does not exist in container {1}.'.format(
+                        blob.name, self.container))
